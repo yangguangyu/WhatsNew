0571a4c27fae0d0cfa905aaa7295e44b0572ae0d
David Levy <davestrand242@gmail.com>
2015-07-31 14:44:13 -0700

    I have changed the local RTF instructions to match some new developments.

--- WhatsNew/MainViewController.swift
+++ WhatsNew/MainViewController.swift
@@ -31,23 +31,23 @@ class MainViewController: UIViewController {
         // (required 1)
         let whatsNewController = WhatsNewController()
         
-        //(optional)
-//        whatsNewController.alertOk = "Yep"
-//        whatsNewController.alertNoThanks = "Nope"
-//        whatsNewController.alertUpdatedToVersion = "Presenting Version "
-//        whatsNewController.alertWouldYouLikeToSeeWhatsNew = "Want to see what is new?"
-//        whatsNewController.showOnFirstLaunch = true
-//        whatsNewController.firstRunOk = "Sounds good."
-//        whatsNewController.firstRunTitle = "Welcome to Happy Pugs!"
-//        whatsNewController.firstRunMessage = "Hey, thanks for trying out Happy Pugs!"
-//        whatsNewController.customModalTransition = UIModalTransitionStyle.PartialCurl
+        //(paste any customizations here)
+        whatsNewController.alertOk = "Yep"
+        whatsNewController.alertNoThanks = "Nope"
+        whatsNewController.alertUpdatedToVersion = "Presenting Version "
+        whatsNewController.alertWouldYouLikeToSeeWhatsNew = "Want to see what is new?"
+        whatsNewController.showOnFirstLaunch = true
+        whatsNewController.firstRunOk = "Sounds good."
+        whatsNewController.firstRunTitle = "Welcome to Happy Pugs!"
+        whatsNewController.firstRunMessage = "Hey, thanks for trying out Happy Pugs!"
+        whatsNewController.customModalTransition = UIModalTransitionStyle.PartialCurl
         whatsNewController.customButtonColor = UIColor.yellowColor()
         whatsNewController.customBackgroundColor = UIColor.redColor()
         
         //(required 2)
-        whatsNewController.appViewController = (self)
-        whatsNewController.alertPageURL = NSURL(string: "https://en.wikipedia.org/wiki/Pug") //edit url
-        whatsNewController.displayFromURLIfNeccessaryInsideApp(true) //edit true or false
+//        whatsNewController.appViewController = (self)
+//        whatsNewController.alertPageURL = NSURL(string: "https://en.wikipedia.org/wiki/Pug") //edit url
+//        whatsNewController.displayFromURLIfNeccessaryInsideApp(true) //edit true or false
         
         
         
@@ -61,33 +61,17 @@ class MainViewController: UIViewController {
         //(required 1)
 //        let whatsNewController = WhatsNewController()
 //        
-        //(optional)
-//        whatsNewController.alertOk = "Sounds good."
-//        whatsNewController.alertUpdatedToVersion = "Presenting Version "
-//        whatsNewController.showOnFirstLaunch = true
+//        //(optional)
+////        whatsNewController.alertOk = "Sounds good."
+////        whatsNewController.alertUpdatedToVersion = "Presenting Version "
+////        whatsNewController.showOnFirstLaunch = true
 //        
-        //(required 2)
+//        //(required 2)
 //        whatsNewController.appViewController = (self)
 //        whatsNewController.alertMessage = "-Pugs no longer deficate when barking.\n-Pugs with three legs now have expected fourth leg in appropriate place.\n-When repositioning collar, Pug's head no longer separates from body."
 //        whatsNewController.displayFromStringIfNecessary()
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //FIXME: Direct access of buttons causing fatal error
-        //Customizing the buttons directly VS using a global color scheme//
-        //Since these buttons might not exist when this page is called how would we access them to change their color?  Perhaps revert to setting a colorState, and assiging that color to all of the buttons
-        //whatsNewController.homeButton.setTitleColor(UIColor .redColor(), forState: UIControlState.Normal)
         
         
     }
